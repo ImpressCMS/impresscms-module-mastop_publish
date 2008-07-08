3e373ea4680c040e66279a87a1f1cbc3c45f5afe
Sina Asghari <stranger@impresscms.org>
2008-07-08 16:27:56 +0000

    added the Persian translation of mastop_publish
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/mastop_publish/trunk@3533 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/persian/blocks.php
@@ -0,0 +1,56 @@
+<?php
+/**
+* Translation of mastop_publish for Persian users
+*
+* @copyright	      http://www.impresscms.ir/ The Persian ImpressCMS Project 
+* @copyright	http://www.irxoops.org/ The Persian XOOPS support site
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @package	      Translations
+* @since		 1.0
+* @author		stranger <pesian_stranger@users.sourceforge.net>
+* @author		http://www.irxoops.org/ The Persian XOOPS support site
+* @version		$Id$
+*/
+define("MPU_BLO_MODDIR", "mastop_publish");
+define("MPU_BLO_OPT_ID", "شماره شناسه بلاک :");
+define("MPU_BLO_OPT_HOME", "نمایش لینک صفحه ی اول؟");
+define("MPU_BLO_OPT_HOMETEXT", "نام لینک صفحه ی اول : ");
+define("MPU_BLO_OPT_MOD", "نمایش ماژول ها؟");
+define("MPU_BLO_OPT_WIDTH", "عرض منو :");
+define("MPU_BLO_OPT_WIDTH_SUB", "عرض زیر منو :");
+define("MPU_BLO_OPT_PADDING", "حاشیه منو : ");
+define("MPU_BLO_OPT_URLARROW", "آدرس تصویر منو های که زیر منو دارند: ");
+define("MPU_BLO_OPT_BGMENU", "رنگ پس زمینه");
+define("MPU_BLO_OPT_BGMENUO", "رنگ پس زمینه با رفتن موشواره بروی منو :");
+define("MPU_BLO_OPT_MENUTCOLOR", "رنگ متن:");
+define("MPU_BLO_OPT_MENUTOCOLOR", "رنگ متن با رفتن موشواره بروی منو: ");
+define("MPU_BLO_OPT_MENUVISITED", "رنگ متن برای لینک های دیده شده:");
+define("MPU_BLO_OPT_FONT", "فونت : ");
+define("MPU_BLO_OPT_FONTO", "فونت با رفتن موشواره بروی منو :");
+define("MPU_BLO_OPT_BOLD", "درشت");
+define("MPU_BLO_OPT_ITALIC", "شکسته");
+define("MPU_BLO_OPT_UNDERLINE", "خط کشی شده");
+define("MPU_BLO_OPT_BORDER", "حاشیه : ");
+define("MPU_BLO_OPT_BORDERO", "رنگ حاشیه با رفتن موشواره بروی منو :");
+define("MPU_BLO_OPT_NONE", "هیچ");
+define("MPU_BLO_OPT_SOLID", "ثابت");
+define("MPU_BLO_OPT_DOUBLE", "دوباره");
+define("MPU_BLO_OPT_OUTSET", "آغاز");
+define("MPU_BLO_OPT_INSET", "جریان");
+define("MPU_BLO_OPT_DASHED", "خط تیره کشیده شده");
+define("MPU_BLO_OPT_DOTTED", "نقطه چین شده");
+define("MPU_BLO_OPT_GROOVE", "یکنواخت");
+define("MPU_BLO_OPT_RIDGE", "مرز");
+
+
+define("MPU_BLO_OPT_FONTSIZE", "سایز متن");
+define("MPU_BLO_OPT_FONTCOLOR", "رنگ متن");
+define("MPU_BLO_OPT_SEPARATOR", "تفکیک کننده");
+
+define("MPU_BLO_SHOW", "نمایش");
+define("MPU_BLO_MPB_30_MENU", "متن منو");
+define("MPU_BLO_MPB_30_TITULO", "عنوان صفحه");
+define("MPU_BLO_OPT_CARACTS", "تعداد کاراکتر های نمایش داده شده برای هر یک از موارد :");
+
+
+?>
---
+++ language/persian/main.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* Translation of mastop_publish for Persian users
+*
+* @copyright	      http://www.impresscms.ir/ The Persian ImpressCMS Project 
+* @copyright	http://www.irxoops.org/ The Persian XOOPS support site
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @package	      Translations
+* @since		 1.0
+* @author		stranger <pesian_stranger@users.sourceforge.net>
+* @author		http://www.irxoops.org/ The Persian XOOPS support site
+* @version		$Id$
+*/
+// V 1.0
+define("MPU_MAI_404", "خطا! صفحه مورد نظر پیدا نشد!");
+define("MPU_MAI_RECOMMEND", "معرفی این صفحه به دوستان!");
+define("MPU_MAI_RECTOAFRIEND", "معرفی صفحه <span style='color:red'>' %s '</span> به یک دوست.");
+define("MPU_MAI_YNAME", "نام شما");
+define("MPU_MAI_YEMAIL", "پست الکترونیکی شما شما");
+define("MPU_MAI_FNAME", "نام دوست شما");
+define("MPU_MAI_FEMAIL", "پست الکترونیکی شما دوست شما");
+define("MPU_MAI_MESSAGE", "پیام معرفی");
+define("MPU_MAI_MAILSUBJECT", "معرفی شده توسط %s");
+define("MPU_MAI_MAILBODY", "سلام %s!<br />%s (%s) این صفحه را ببینید ' <a href='%s'>%s</a> ' and would like to recommend it to you.<br />In case you're not being able to see the link, copy and paste it at your browser: <br /> %s <br /><br />Additional message from %s:<br/>%s");
+define("MPU_MAI_MAILSUCCESS", "<h3>پیام به %s ارسال شد.</h3><a href='%s'>بازگشت</a>");
+define("MPU_MAI_PRINT", "آماده کردن برای چاپ");
+define("MPU_MAI_INFOPG", "اطلاعات");
+define("MPU_MAI_AUTOR", "نویسنده صفحه:");
+define("MPU_MAI_CRIADO", "تاریخ ساخت:");
+define("MPU_MAI_ATUALIZADO", "آخرین به روز رسانی:");
+define("MPU_MAI_CONTADOR", "این صفحه <b>%u</b> بار دیده شده");
+define("MPU_MAI_ZCONTADOR", "صفر کردن بازدید ها");
+define("MPU_MAI_EDITPAGE", "ویرایش صفحه");
+define("MPU_MAI_RELATED", "برای اطلاعات بیشتر به صفحات زیر نیز سری بزنید");
+//V1.1
+define("MPU_MAI_NEWPAGE", "زیر صفحه جدید");
+define("MPU_MAI_MYPAGES", "صفحات من");
+?>
